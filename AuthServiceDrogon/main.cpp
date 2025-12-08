f5db8b708d8ed810d4a29f8ce25ca88bd3201cd2
Arthur <artsadert@gmail.com>
2025-12-08 14:16:41 +0300

    revise origins to local network in auth service

--- AuthServiceDrogon/main.cpp
+++ AuthServiceDrogon/main.cpp
@@ -5,29 +5,34 @@ int main() {
   drogon::app().loadConfigFile("config.json");
 
   // Add CORS support for frontend
-  drogon::app().registerPreRoutingAdvice([](const drogon::HttpRequestPtr &req,
-                                           drogon::AdviceCallback &&acb,
-                                           drogon::AdviceChainCallback &&accb) {
-    // Handle OPTIONS preflight requests
-    if (req->method() == drogon::HttpMethod::Options) {
-      auto resp = drogon::HttpResponse::newHttpResponse();
-      resp->addHeader("Access-Control-Allow-Origin", "*");
-      resp->addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-      resp->addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-      resp->addHeader("Access-Control-Max-Age", "86400");
-      acb(resp);
-      return;
-    }
-    accb();
-  });
+  drogon::app().registerPreRoutingAdvice(
+      [](const drogon::HttpRequestPtr &req, drogon::AdviceCallback &&acb,
+         drogon::AdviceChainCallback &&accb) {
+        // Handle OPTIONS preflight requests
+        if (req->method() == drogon::HttpMethod::Options) {
+          auto resp = drogon::HttpResponse::newHttpResponse();
+          resp->addHeader("Access-Control-Allow-Origin", "*");
+          resp->addHeader("Access-Control-Allow-Methods",
+                          "GET, POST, PUT, DELETE, OPTIONS");
+          resp->addHeader("Access-Control-Allow-Headers",
+                          "Content-Type, Authorization");
+          resp->addHeader("Access-Control-Max-Age", "86400");
+          acb(resp);
+
+          return;
+        }
+        accb();
+      });
 
   // Add CORS headers to all responses
-  drogon::app().registerPostHandlingAdvice([](const drogon::HttpRequestPtr &,
-                                             const drogon::HttpResponsePtr &resp) {
-    resp->addHeader("Access-Control-Allow-Origin", "*");
-    resp->addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    resp->addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  });
+  drogon::app().registerPostHandlingAdvice(
+      [](const drogon::HttpRequestPtr &, const drogon::HttpResponsePtr &resp) {
+        resp->addHeader("Access-Control-Allow-Origin", "*");
+        resp->addHeader("Access-Control-Allow-Methods",
+                        "GET, POST, PUT, DELETE, OPTIONS");
+        resp->addHeader("Access-Control-Allow-Headers",
+                        "Content-Type, Authorization");
+      });
 
   LOG_DEBUG << "running on localhost:3000";
   drogon::app().run();
